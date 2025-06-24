69ac769346a1a1bb7b04c41f071c511e526d09b0
sofiatorres25 <sofiatorres5082@gmail.com>
2025-06-24 17:50:57 -0300

    feat: agregar relaciones a los modelos

--- backend/app/Models/Socio.php
+++ backend/app/Models/Socio.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Socio extends Model
 {
-    //
+    // Un socio puede tener muchos talleres a través de la tabla intermedia socio_taller
+    public function talleres()
+    {
+        return $this->belongsToMany(Taller::class, 'socio_taller');
+    }
 }
--- backend/app/Models/Taller.php
+++ backend/app/Models/Taller.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Taller extends Model
 {
-    //
+    // Un taller puede tener muchos socios a través de la tabla intermedia socio_taller
+    public function socios()
+    {
+        return $this->belongsToMany(Socio::class, 'socio_taller');
+    }
 }
